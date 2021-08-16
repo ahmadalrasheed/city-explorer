283181981cdfd5d7e4b2297ad85f3cc21deff936
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-08-16 23:24:29 +0300

    applying heroku on netlify

--- src/App.js
+++ src/App.js
@@ -6,34 +6,42 @@ import FormDisplay from './components/form';
 import TableDisplay from './components/table';
 import MapDisplay from './components/location';
 import './components/style.css';
+// import { NavItem } from 'react-bootstrap';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      weatherdata:[],
+      weatherdescription:'',
       Mapimage:'',
       displaytable:'',
       location: {},
       displayname:'',
       Latitude:'',
-      Longitude:''
+      Longitude:'',
+
     }
   }
 
   ShowLocations = async (e) => {
     e.preventDefault();
     let result = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_YOUR_ACCESS_TOKEN}&q=${e.target.Explorer.value}&format=json`);
+    let Weather = await axios.get(`http://localhost:3001/weatherdata?city_name=${e.target.Explorer.value}`);
+    console.log(Weather.data);
+    
     
     await this.setState({
+      weatherdata:Weather.data,
       displaytable:1,
       location: result,
       displayname:result.data[0].display_name,
       Latitude:result.data[0].lat,
-      Longitude:result.data[0].lon
+      Longitude:result.data[0].lon,
+
     })
     // console.log(this.state.location.data[0]);
-    
     
   }
   
@@ -48,6 +56,18 @@ class App extends React.Component {
 
       {this.state.displaytable && <img alt='city' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_YOUR_ACCESS_TOKEN}&center=${this.state.Latitude},${this.state.Longitude}&zoom=12`}/>}
 
+      {this.state.weatherdata.forEach(item =>{
+        return(
+          <>
+          {this.state.displaytable && <p> {item.date}</p>}
+          {this.state.displaytable && <p>{item.description}</p>}
+          
+          </>
+        )
+
+        
+      })}
+      
       </>
     )
   }
